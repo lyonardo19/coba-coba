9340616056e79024c5390fe7fd1cbcedde878847
ASUS <lyonardo.madrid@gmail.com>
2022-04-26 14:30:10 +0700

    api automation

---
+++ src/test/java/starter/stepdefinitions/UserSteps.java
@@ -0,0 +1,30 @@
+package starter.stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.thucydides.core.annotations.Steps;
+import starter.user.Get;
+
+public class UserSteps {
+    @Steps
+    Get get;
+
+    @Given("I set GET api endpoint")
+    public void setGetApiEndpoint(){
+        get.setApiEndpoint();
+    }
+    @When("I send GET HTTP request")
+    public void sendGetHttpRequest(){
+        get.sendGetHttpRequest();
+    }
+    @Then("I receive valid HTTP response code 200")
+    public void receiveValidHttpResponse(){
+        get.receiveValidHttpResponse();
+    }
+    @And("I receive valid data for detail user")
+    public void receiveValidDataForDetailUser(){
+        get.receiveValidDataForDetailUSer();
+    }
+}
